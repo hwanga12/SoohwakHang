30df960c571b479885163d0752a2ba55ddb7e1d1
gayeon <ghkdrkdus1@naver.com>
2026-03-04 17:54:19 +0900

    S14P21A602-3 test3

--- tools/git-hooks/install.sh
+++ tools/git-hooks/install.sh
@@ -3,4 +3,4 @@ set -e
 mkdir -p .git/hooks
 cp tools/git-hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
 chmod +x .git/hooks/prepare-commit-msg
-echo "✅ Git hook installed"
+echo "✅ Git hook installed"  
