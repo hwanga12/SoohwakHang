5cb5df109b0415d4be31432d652ada46dee8884b
leejaeyup <leejaeyup0317@gmail.com>
2026-03-13 09:58:12 +0900

    기본 오브젝트 배치

--- agribot_ws/src/agribot_description/launch/spawn_agribot.launch.py
+++ agribot_ws/src/agribot_description/launch/spawn_agribot.launch.py
@@ -7,9 +7,9 @@ Usage:
 """
 
 import os
-from ament_index_packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
@@ -18,6 +18,17 @@ def generate_launch_description():
     # Package paths
     pkg_agribot_description = get_package_share_directory('agribot_description')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    # Set Gazebo resource path to find models
+    # This adds the share/agribot_description/models directory to GZ_SIM_RESOURCE_PATH
+    gz_resource_path = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=[
+            os.path.join(pkg_agribot_description, 'models'),
+            ':',
+            os.getenv('GZ_SIM_RESOURCE_PATH', '')
+        ]
+    )
 
     # Launch arguments
     world_arg = DeclareLaunchArgument(
@@ -39,6 +50,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        gz_resource_path,
         world_arg,
         gz_sim,
     ])
