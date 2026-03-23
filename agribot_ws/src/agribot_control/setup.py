e9b137398cbc13b598862bee7bb5cd5bbbb4b875
leejaeyup <leejaeyup0317@gmail.com>
2026-03-23 17:26:01 +0900

    커튼과 환기 실행 판단 로직을 환경 규칙으로 구현한다

--- agribot_ws/src/agribot_control/setup.py
+++ agribot_ws/src/agribot_control/setup.py
@@ -26,6 +26,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'climate_decision_node = agribot_control.climate_decision_node:main',
             'mission_manager = agribot_control.mission_manager:main',
             'watering_decision_node = agribot_control.watering_decision_node:main',
         ],
